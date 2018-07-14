289ccb791083393c48b2aacc63c17dda765325ea
aishwarya <chennur1997@gmail.com>
2018-07-14 21:18:19 +0530

    updatedlogin

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.aishupriya.youthforseva;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt=(Button)findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent it=new Intent(MainActivity.this,Act_login.class);
+                startActivity(it);
+            }
+        });
     }
 }
---
+++ java/dash_volunteer.java
@@ -0,0 +1,14 @@
+package com.example.aishupriya.youthforseva;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class dash_volunteer extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_volunteer);
+    }
+
+}
