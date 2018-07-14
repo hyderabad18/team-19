79f785a380fb5447cc020c6efae7949bedf09010
varun5298 <varunpatlolla5298@gmail.com>
2018-07-14 23:37:00 +0530

    modified constants.php

--- backend/include/Constants.php
+++ backend/include/Constants.php
@@ -1,6 +1,6 @@
 <?php
 //Constants to connect with the database
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Test@123');
+define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'yfs');
